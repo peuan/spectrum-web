f2679e22e12ee352db28b91544bd0cd52d964180
joji000 <172300802+joji000@users.noreply.github.com>
2024-12-23 17:45:01 +0700

    feat: refactor card components

---
+++ src/app/card_test/page.tsx
@@ -0,0 +1,136 @@
+import LoginCard from '@/components/LoginCard'
+import Paymentcard from '@/components/PaymentCard'
+import ReviewsCard from '@/components/ReviewsCard'
+import { Container, Box, Grid2 } from '@mui/material'
+
+const Page = () => {
+  return (
+    <>
+      <Box
+        sx={{
+          height: '100vh',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          backgroundImage: 'url(/bg-1.svg)',
+          backgroundSize: 'cover',
+          backgroundPosition: 'center',
+          position: 'relative',
+          overflow: 'hidden',
+        }}
+      >
+        {/*login card*/}
+        <LoginCard />
+      </Box>
+
+      {/*payment card*/}
+      <Container>
+        <Grid2
+          spacing={2}
+          container
+          sx={{
+            pt: 8,
+          }}
+        >
+          <Grid2
+            size={{
+              xs: 12,
+              md: 6,
+              lg: 6,
+            }}
+          >
+            <Paymentcard
+              title="TrueMoney Voucher"
+              quote1="ทรูมันนี่ อังเปา"
+              quote2="กับเบอร์ที่คุณยืนยัน"
+            />
+          </Grid2>
+
+          <Grid2
+            size={{
+              xs: 12,
+              md: 6,
+              lg: 6,
+            }}
+          >
+            <Paymentcard
+              title="Verify Slip"
+              quote1="ยืนยันสลิปธนาคาร"
+              quote2="กับที่อยู่การรับเงินที่คุณตั้งค่าไว้"
+            />
+          </Grid2>
+
+          <Grid2
+            size={{
+              xs: 12,
+              md: 6,
+              lg: 6,
+            }}
+          >
+            <Paymentcard
+              title="PromptPay"
+              quote1="ยืนยันสลิปพร้อมเพย์"
+              quote2="กับที่อยู่การรับเงินที่คุณตั้งค่าไว้"
+            />
+          </Grid2>
+        </Grid2>
+      </Container>
+
+      {/*reviews card*/}
+      <Container>
+        <Grid2
+          spacing={2}
+          container
+          sx={{
+            pt: 8,
+          }}
+        >
+          <Grid2
+            size={{
+              xs: 12,
+              md: 6,
+              lg: 4,
+            }}
+          >
+            <ReviewsCard
+              score={5}
+              comment="Lorem ipsum dolor sit amet consectetur. Faucibus fermentum donec quam donec arcu leo posuere tempor feugiat. Odio eu velit feugiat mi est. Urna odio in senectus pellentesque lacus varius. Et sed."
+              profilePic="/icons/test-profilepic.svg"
+              name="Robert L."
+            />
+          </Grid2>
+          <Grid2
+            size={{
+              xs: 12,
+              md: 6,
+              lg: 4,
+            }}
+          >
+            <ReviewsCard
+              score={4}
+              comment="Lorem ipsum dolor sit amet consectetur. Faucibus fermentum donec quam donec arcu leo posuere tempor feugiat. Odio eu velit feugiat mi est. Urna odio in senectus pellentesque lacus varius. Et sed."
+              profilePic="/icons/test-profilepic.svg"
+              name="Robert L."
+            />
+          </Grid2>
+          <Grid2
+            size={{
+              xs: 12,
+              md: 6,
+              lg: 4,
+            }}
+          >
+            <ReviewsCard
+              score={4.5}
+              comment="Lorem ipsum dolor sit amet consectetur. Faucibus fermentum donec quam donec arcu leo posuere tempor feugiat. Odio eu velit feugiat mi est. Urna odio in senectus pellentesque lacus varius. Et sed."
+              profilePic="/icons/test-profilepic.svg"
+              name="Robert L."
+            />
+          </Grid2>
+        </Grid2>
+      </Container>
+    </>
+  )
+}
+
+export default Page
--- src/components/LoginCard.tsx
+++ src/components/LoginCard.tsx
@@ -1,123 +1,55 @@
 'use client'
 
 import React from 'react'
-import {
-  Avatar,
-  Box,
-  Typography,
-  Button,
-  Card,
-  CardContent,
-  Stack,
-} from '@mui/material'
+import { Avatar, Typography, Card, CardContent, Stack } from '@mui/material'
+import CustomLoginButton from './CustomLoginButton'
 
 const LoginCard: React.FC = () => {
   return (
     <Card
       sx={{
-        width: 572,
-        height: 482,
-        borderRadius: '10px',
+        width: '100%',
+        maxWidth: 572,
         bgcolor: '#1C1D1F',
         display: 'flex',
         flexDirection: 'column',
         justifyContent: 'center',
         alignItems: 'center',
+        padding: 3,
       }}
     >
       {/* Logo */}
 
       <Avatar
         sx={{
-          width: 258,
-          height: 258,
-          mt: '-17px',
+          width: '100%',
+          maxWidth: 258,
+          height: '100%',
+          maxHeight: 258,
         }}
         src="/icons/full-logo.svg"
       ></Avatar>
 
       {/* Content */}
-      <CardContent>
-        <Typography
-          marginTop="-35px"
-          fontSize="16px"
-          color="#ffffff"
-          textAlign="left"
-        >
+      <CardContent
+        sx={{
+          width: '100%',
+        }}
+      >
+        <Typography mt={-4} variant="body2">
           Sign in to your Account
         </Typography>
 
         {/* Buttons */}
-        <Stack gap={2} marginTop={2}>
-          <Button
-            sx={{
-              width: 461,
-              height: 60,
-              borderRadius: 10,
-              bgcolor: '#303030',
-              color: 'white',
-              fontSize: '16px',
-              fontWeight: '400',
-              textTransform: 'none',
-              '&:hover': { bgcolor: '#3A3A3C' },
-            }}
-          >
-            <Box
-              component="span"
-              sx={{
-                display: 'flex',
-                alignItems: 'center',
-                justifyContent: 'center',
-              }}
-            >
-              <Avatar src="/icons/google-logo.svg"></Avatar>
-            </Box>
-            <Typography
-              component="span"
-              sx={{
-                flexGrow: 1,
-                textAlign: 'center',
-                marginRight: '40px',
-              }}
-            >
-              Continue with Google
-            </Typography>
-          </Button>
-
-          <Button
-            sx={{
-              width: 461,
-              height: 60,
-              borderRadius: 10,
-              bgcolor: '#303030',
-              color: 'white',
-              fontSize: '16px',
-              fontWeight: '400',
-              textTransform: 'none',
-              '&:hover': { bgcolor: '#3A3A3C' },
-            }}
-          >
-            <Box
-              component="span"
-              sx={{
-                display: 'flex',
-                alignItems: 'center',
-                justifyContent: 'center',
-              }}
-            >
-              <Avatar src="/icons/apple-logo.svg"></Avatar>
-            </Box>
-            <Typography
-              component="span"
-              sx={{
-                flexGrow: 1,
-                textAlign: 'center',
-                marginRight: '40px',
-              }}
-            >
-              Continue with Apple
-            </Typography>
-          </Button>
+        <Stack spacing={2} mt={2}>
+          <CustomLoginButton
+            logo="/icons/google-logo.svg"
+            text="Continue with Google"
+          />
+          <CustomLoginButton
+            logo="/icons/apple-logo.svg"
+            text="Continue with Apple"
+          />
         </Stack>
       </CardContent>
     </Card>
